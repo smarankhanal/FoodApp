2c659268cc457a436ebb97c7a2804bbdba5c7c70
smarankhanal <smkhanal123@gmail.com>
2025-07-24 09:19:35 +0545

    utilis

---
+++ backend/src/utlis/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      throw new Error("Local file path not provided.");
+    }
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    console.error("Cloudinary upload error:", error.message);
+    return null;
+  }
+};
+export default uploadOnCloudinary;
