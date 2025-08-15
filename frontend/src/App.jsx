df7ecb070d33a1973160da28e0b6f1f7a26b8f6e
smarankhanal <smkhanal123@gmail.com>
2025-08-15 22:01:55 +0545

    admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Footer, NavBar } from "./components";
-import {
-  FoodItems,
-  FoodPage,
-  OrderItems,
-  Settings,
-  User,
-  UserHistory,
-} from "./pages";
+import { SingleOrderHistory } from "./pages";
 export default function App() {
   return (
     <div className="relative">
       <NavBar />
-      {/* <Ordered /> */}
-      {/* <SingleOrderHistory /> */}
-      {/* <FoodItems /> */}
-      {/* <OrderItems /> */}
-      {/* <Settings /> */}
-      {/* <FoodPage /> */}
-      {/* <User /> */}
-      <UserHistory />
+      <SingleOrderHistory />
       <Footer />
     </div>
   );
--- frontend/src/pages/Order/SingleOrderHistory.jsx
+++ frontend/src/pages/Order/SingleOrderHistory.jsx
@@ -5,16 +5,18 @@ export default function SingleOrderHistory() {
   return (
     <div className="bg-[url('/images/light.jpg')] dark:bg-[url('/images/dark.jpg')]  bgImage pt-20">
       <div className=" w-full max-w-3xl mx-auto dark:text-white mb-10 text-[20px] font-serif font-bold">
-        <div className="grid grid-cols-3 gap-4 bg-gray-200 dark:bg-gray-700 border border-gray-400 dark:border-gray-600 rounded-t-md px-4 py-2">
-          <div>Order Id</div>
-          <div>Date</div>
-          <div>Total Price</div>
+        <div className="flex bg-amber-500 rounded-lg mb-3 p-1">
+          <p className="flex-1">Order Id</p>
+          <p className="flex-1">Date</p>
+          <p className="flex-1">Status</p>
+          <p className="flex-1">Total Price</p>
         </div>
 
-        <div className="grid grid-cols-3 gap-4 border border-t-0 border-gray-400 dark:border-gray-600 rounded-b-md px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-800">
-          <div>12345</div>
-          <div>2025-08-09</div>
-          <div>$150</div>
+        <div className="flex dark:bg-white bg-black dark:text-black text-white rounded-lg mb-2 p-1">
+          <p className="flex-1">12345</p>
+          <p className="flex-1">2025-08-09</p>
+          <p className="flex-1">Completed</p>
+          <p className="flex-1">$150</p>
         </div>
       </div>
 
