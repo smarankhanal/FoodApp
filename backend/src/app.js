b45cc2b4d34d7b8239d7ea0881b99f23aeb04bc6
smarankhanal <smkhanal123@gmail.com>
2025-08-28 20:47:38 +0545

    adjusting the backend for depolying

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,80 +1,38 @@
-// import express from "express";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import { errorHandler } from "./middlewares/errorHandler.js";
-
-// const app = express();
-// const allowedOrigins = process.env.CORS_ORIGIN.split(",");
-
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-
-// app.use(express.json({ limit: "16kb" }));
-// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-// app.use(express.static("public"));
-// app.use(cookieParser());
-
-// import userRouter from "./routes/user.routes.js";
-
-// import adminRouter from "./routes/admin.routes.js";
-
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/admin", adminRouter);
-// app.use(errorHandler);
-
-// export default app;
-
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import userRouter from "./routes/user.routes.js";
-import adminRouter from "./routes/admin.routes.js";
 
 const app = express();
-const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+const allowedOrigins = ["https://food-app-sigma-bay.vercel.app"];
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.options(
-  "*",
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.options("*", (req, res) => {
+  res.set({
+    "Access-Control-Allow-Origin": "https://food-app-sigma-bay.vercel.app",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Credentials": "true",
+  });
+  res.sendStatus(200);
+});
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+
+import adminRouter from "./routes/admin.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/admin", adminRouter);
