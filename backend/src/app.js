8b8d7ffc14db32f359cf4c36267a5c87368b7f34
smarankhanal <smkhanal123@gmail.com>
2025-08-28 13:35:04 +0545

    adjusting the backend for depolying

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,10 +19,6 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  console.log("🔍 Incoming Origin:", req.headers.origin);
-  next();
-});
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
