a5a1e73f89438ec230aa6a9db6716e1b50e8bad3
smarankhanal <smkhanal123@gmail.com>
2025-08-28 20:43:32 +0545

    adjusting the backend for depolying

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,64 @@
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import { errorHandler } from "./middlewares/errorHandler.js";
+
+// const app = express();
+// const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// app.use(express.static("public"));
+// app.use(cookieParser());
+
+// import userRouter from "./routes/user.routes.js";
+
+// import adminRouter from "./routes/admin.routes.js";
+
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/admin", adminRouter);
+// app.use(errorHandler);
+
+// export default app;
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import userRouter from "./routes/user.routes.js";
+import adminRouter from "./routes/admin.routes.js";
 
 const app = express();
 const allowedOrigins = process.env.CORS_ORIGIN.split(",");
-console.log("Allowed Origins:", allowedOrigins);
-app.use((req, res, next) => {
-  console.log("🔍 Origin:", req.headers.origin);
-  next();
-});
 
 app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.options(
+  "*",
   cors({
     origin: (origin, callback) => {
       if (!origin || allowedOrigins.includes(origin)) {
@@ -28,10 +75,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
-import userRouter from "./routes/user.routes.js";
-
-import adminRouter from "./routes/admin.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/admin", adminRouter);
