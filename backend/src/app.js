09576f60b6aeb7d5679a4dc607fa5681c9985d7f
smarankhanal <smkhanal123@gmail.com>
2025-08-29 16:05:05 +0545

    adjusting the backend for depolying

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,14 +4,31 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
-const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+// const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",").map((origin) =>
+  origin.trim().replace(/\/$/, "")
+);
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin.replace(/\/$/, ""))) {
         callback(null, true);
       } else {
+        console.warn(`Blocked by CORS: ${origin}`);
         callback(new Error("Not allowed by CORS"));
       }
     },
@@ -19,6 +36,8 @@ app.use(
   })
 );
 
+// Handle preflight
+app.options("*", cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
