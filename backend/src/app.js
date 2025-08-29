e14da800067f0bb7444e241b56157ac96594aa24
smarankhanal <smkhanal123@gmail.com>
2025-08-29 16:21:09 +0545

    adjusting the backend for depolying

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,10 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
+app.use((req, res, next) => {
+  console.log("Incoming Origin:", req.headers.origin);
+  next();
+});
 const allowedOrigins = process.env.CORS_ORIGIN.split(",");
 
 app.use(
@@ -18,11 +22,6 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  console.log("Incoming Origin:", req.headers.origin);
-  next();
-});
-
 app.options("*", cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
