e8c6bfc3b415b7fd93ba6f73f3dfd055a19c511e
smarankhanal <smkhanal123@gmail.com>
2025-09-17 17:15:59 +0545

    mobile layout frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import "./App.css";
 import { DarkModeInitializer, Footer, NavBar } from "./components";
 import { Outlet } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { fetchUser } from "./store/authSlice";
+
 export default function App() {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchUser());
-  // }, []);
   return (
     <div className="relative">
       <DarkModeInitializer />
