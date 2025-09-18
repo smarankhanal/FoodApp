f69160793ffdde59b163b92e0b68007f7dc6a88f
smarankhanal <smkhanal123@gmail.com>
2025-09-18 08:20:47 +0545

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,15 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 import app from "./app.js";
-
-dotenv.config({ path: "./env" });
-
-await connectDB();
-export default app;
-
-// .then(() => {
-//   app.listen(process.env.PORT || 8000, () => {
-//     console.log(`Server  is running at port ${process.env.PORT}`);
-//   });
-// })
-// .catch((err) => {
-//   console.log("MongoDB error", err);
-// });
+dotenv.config({
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server  is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB error", err);
+  });
