f2cc73dddce4ba8b5650f07a391d79fafe1afe48
smarankhanal <smkhanal123@gmail.com>
2025-09-18 14:09:17 +0545

    changes for depolyment

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -61,7 +61,7 @@ export default function LogIn() {
         )}
 
         <div className="flex items-center justify-center">
-          <Button className="m-2 bg-blue-700" disabled={loading}>
+          <Button className="m-2 bg-blue-700">
             {/* {loading ? "Logging in..." : "LogIn"} */}
             LogIn
           </Button>
