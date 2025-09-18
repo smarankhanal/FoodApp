e5e612f40ce060fc041b13c2840625a3a1fcb247
smarankhanal <smkhanal123@gmail.com>
2025-09-18 22:10:38 +0545

    final changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,6 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
