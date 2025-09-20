47c2eef5f66b4099410339878012f1ffb94947d1
smarankhanal <smkhanal123@gmail.com>
2025-09-20 22:21:15 +0545

    foodapp sucessfully made

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,23 +1,3 @@
-// import axios from "axios";
-
-// const API_URL = import.meta.env.VITE_API_URL;
-
-// const api = axios.create({
-//   baseURL: API_URL,
-//   withCredentials: true,
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -29,31 +9,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const { data } = await api.post("/users/refresh-token");
-        const newAccessToken = data?.data?.accessToken;
-        if (newAccessToken) {
-          localStorage.setItem("token", newAccessToken);
-          api.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
-          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        }
-        return api(originalRequest);
-      } catch (refreshError) {}
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default api;
